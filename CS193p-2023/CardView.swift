112734ed34972a01838a3d0d0580dd93abf489eb
baifumei <k_katerina_k@icloud.com>
2023-11-07 18:35:13 +0300

    Move CardView to a new file

---
+++ CS193p-2023/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  CS193p-2023
+//
+//  Created by Екатерина К on 07.11.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 25)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 3)
+                Text(card.content)
+                    .font(.system(size: 50))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }.opacity(card.isFaceUp ? 1 : 0)
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+
+
+
+
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(MemoryGame<String>.Card(content: "X", id: "test1"))
+            .padding()
+            .foregroundColor(.pink)
+    }
+}
--- CS193p-2023/EmojiMemoryGameView.swift
+++ CS193p-2023/EmojiMemoryGameView.swift
@@ -57,42 +57,6 @@ struct EmojiMemoryGameView: View {
 
 
 
-struct CardView: View {
-    var card: MemoryGame<String>.Card
-    
-    init(_ card: MemoryGame<String>.Card) {
-        self.card = card
-    }
-    
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 25)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 3)
-                Text(card.content)
-                    .font(.system(size: 50))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode: .fit)
-            }.opacity(card.isFaceUp ? 1 : 0)
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
